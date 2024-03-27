efb2cee7453a0a33cb1f7099972320264f050fab
Techscool <100423240+krishna6547@users.noreply.github.com>
2024-03-27 10:45:51 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>   
+#include <time.h>
+void delay(int ms) {
+    clock_t start_time = clock();
+    double milliseconds = ms * (CLOCKS_PER_SEC / 1000.0);
+
+    while ((clock() - start_time) < milliseconds);
+}
+void main()
+{
+// clrscr(); clear previous output (optional if you use turboc++)
+  int boxes, loop;
+  printf("How many repeatitions: ");
+  scanf("%d",&loop);
+  for (int times =1; times <=loop; times++)
+  {
+    for (boxes = 1; boxes <=10; boxes++)
+    {
+
+      for (int column = 1; column <=boxes; column++)//print increasin boxes
+      {
+        for (int row =1 ; row<=boxes; row++)
+          printf("*");
+        printf("\n");// seperate lines
+      }
+      printf("\n"); // separate boxes
+      delay(100);
+    }
+    for (boxes = 1; boxes<=10; boxes++)
+    {
+      for (int column = boxes; column <=10; column++)  // print decresing boxes
+      {
+        for (int row =boxes ; row<=10; row++)
+          printf("*");
+        printf("\n");// 	seperate lines
+      }
+      printf("\n"); // separate boxes
+      delay(100);
+    }
+
+  }
+  //getchar(); //stay at output menu (optional if you use turboc++)
+}
